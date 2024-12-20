ef19fd35dd630e9fdb2017a18dd043140d79ac2f
williamberggrn <william.berggren@elev.ga.lbs.se>
2024-12-20 11:03:55 +0100

    no jokes about pushing or pulling anymore

--- There's a fish in my book/Assets/William B/Scripts/Item.cs
+++ There's a fish in my book/Assets/William B/Scripts/Item.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    // Attributes
+
+
+    // Variables
+
+
+    // References
+
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
