538738efe9ea5d0fab43936999b902baf2ce2777
williamberggrn <william.berggren@elev.ga.lbs.se>
2025-01-24 10:16:04 +0100

    clinkoclanko

--- There's a fish in my book/Assets/William B/Scripts/ControllerInputHandler.cs
+++ There's a fish in my book/Assets/William B/Scripts/ControllerInputHandler.cs
@@ -1,52 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.XR;
-using TMPro;
 
-public class Book : MonoBehaviour
+public class ControllerInputHandler : MonoBehaviour
 {
-    // Attributes
-    
-
-    // Variables
-    private bool isOpen = false;
-    public bool IsOpen
-    {
-         get {return isOpen; }
-    }
-
-    // Reference
-    [SerializeField] private TextMeshPro title, author;
-    [SerializeField] private Sprite genre;
-    [SerializeField] private Material color;
-    [SerializeField] UnityEvent onOpenEvents;
     private InputDevice leftController, rightController;
-    public Book(TextMeshPro nTitle, TextMeshPro nAuthor, Sprite nGenre, Material nColor, UnityEvent newEvents = null)
-    {
-        title = nTitle;
-        author = nAuthor;
-        genre = nGenre;
-        color = nColor;
-        onOpenEvents = newEvents;
-    }
 
-    /// <summary>
-    /// Gets triggered when the book is opened, not when picked up
-    /// </summary>
-    public void OnOpen()
-    {
-        isOpen = true;
-
-        // If the book is a fake (if it has events), trigger said events
-        if (onOpenEvents != null)
-        {
-            onOpenEvents.Invoke();
-        }
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
         // Hämta de enheter som motsvarar de vänstra och högra kontrollerna
@@ -66,13 +26,12 @@ public class Book : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         // Kolla om knappen är nedtryckt på vänster kontroll
         if (leftController.isValid && leftController.TryGetFeatureValue(CommonUsages.primaryButton, out bool isPressed) && isPressed)
         {
-            
+            Debug.Log("Vänster knapp trycks på.");
         }
 
         // Kolla om knappen är nedtryckt på höger kontroll
