1f99d5b5390958d628ff5469c2574d08edce28d4
Tristan Blankenfjell <tristan.blankenfjel@learnet.se>
2025-03-25 13:19:22 +0100

    did some shit

--- There's a fish in my book/Assets/Tristan/tristans scripts/fishscript.cs
+++ There's a fish in my book/Assets/Tristan/tristans scripts/fishscript.cs
@@ -7,6 +7,10 @@ public class fishscript : MonoBehaviour
     public HingeJoint tailJoint;
     public float swimForce = 5f;
     public float swimSpeed = 3f;
+    public bool moves;
+    public bool makeSound;
+    public AudioSource[] sounds = new AudioSource[5];
+
 
     void Start()
     {
@@ -15,12 +19,24 @@ public class fishscript : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Apply sinusoidal movement to the joint motor
-        JointMotor motor = tailJoint.motor;
-        motor.force = swimForce;
-        motor.targetVelocity = Mathf.Sin(Time.time * swimSpeed) * 100;
-        motor.freeSpin = false;
-        tailJoint.motor = motor;
-        tailJoint.useMotor = true;
+        if (moves)
+        {
+            // Apply sinusoidal movement to the joint motor
+            JointMotor motor = tailJoint.motor;
+            motor.force = swimForce;
+            motor.targetVelocity = Mathf.Sin(Time.time * swimSpeed) * 100;
+            motor.freeSpin = false;
+            tailJoint.motor = motor;
+            tailJoint.useMotor = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (makeSound)
+        {
+            int rn = Random.Range(0, sounds.Length);
+            sounds[rn].Play();
+        }
     }
 }
