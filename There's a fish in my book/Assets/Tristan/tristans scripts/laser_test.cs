580b01ddffeb8b77e9357a371226ec2e56db832c
Tristan Blankenfjell <tristan.blankenfjel@learnet.se>
2025-03-25 10:31:57 +0100

    sabers

--- There's a fish in my book/Assets/Tristan/tristans scripts/laser_test.cs
+++ There's a fish in my book/Assets/Tristan/tristans scripts/laser_test.cs
@@ -2,21 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-//[CustomEditor(typeof(laser_VFX_script))]
-public class laser_tes
+#if UNITY_EDITOR
+[CustomEditor(typeof(laser_VFX_script))]
+public class laser_test : Editor
 {
-    /*
+    
     public override void OnInspectorGUI()
     {
-        //base.OnInspectorGUI();
+        base.OnInspectorGUI();
 
-        //serializedObject.UpdateIfRequiredOrScript();
+        serializedObject.UpdateIfRequiredOrScript();
 
-        //laser_VFX_script script = (laser_VFX_script)target;
+        laser_VFX_script script = (laser_VFX_script)target;
 
         if (GUILayout.Button("Ya mom"))
-            //script.SwitchSaber();
-        //serializedObject.ApplyModifiedProperties();
+            script.SwitchSaber();
+        serializedObject.ApplyModifiedProperties();
     }
-    */
+    
 }
+#endif
